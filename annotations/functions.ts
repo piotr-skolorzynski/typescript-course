558242f750c0da04c88ab43371ff80827b5309ac
Piotr <p.skolorzynski@gmail.com>
2022-01-05 22:55:09 +0100

    create functions.ts file add example of using functions with annotations, using destructurization to pass arguments

---
+++ annotations/functions.ts
@@ -0,0 +1,42 @@
+const add = (a: number, b: number): number => {
+    return a + b;
+};
+
+const substract = (a: number, b: number): number => {
+    return a - b;
+};
+
+function divide (a: number, b: number): number {
+    return a/b;
+};
+
+const multiply = function(a: number, b: number): number {
+    return a * b;
+};
+
+const logger = (message: string): void => {
+    console.log(message);
+};
+
+// never means that functions never ends so it cannot return any value
+const throwError = (message: string): never => {
+    throw new Error(message);
+};
+
+const todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = (forecast: { date: Date, weather: string}): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+}
+
+// ES2015
+const logWeather2 = ({ date, weather }: { date: Date, weather: string }) => {
+    console.log(date);
+    console.log(weather);
+}
+
+logWeather(todaysWeather);
