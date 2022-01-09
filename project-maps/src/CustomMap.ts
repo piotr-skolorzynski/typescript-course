9561e97c83583249b41b1553f56e0c9cba35f239
Piotr <p.skolorzynski@gmail.com>
2022-01-09 22:11:08 +0100

    create CustomMap.ts with class modify index.ts

---
+++ project-maps/src/CustomMap.ts
@@ -0,0 +1,31 @@
+import { Company } from './Company';
+import { User } from './User';
+///<reference types="@types/google.maps" />
+
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+
+    addUserMarker(user: User):void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng
+            }
+        })
+    }
+
+    addCompanyMarker(company: Company): void{
+
+    }
+}
--- project-maps/src/index.ts
+++ project-maps/src/index.ts
@@ -1,12 +1,9 @@
-/// <reference types="@types/google.maps" />
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from './User';
+///<reference types="@types/google.maps" />
 
-//check defintiotion, it can take html element and options
-new google.maps.Map(document.getElementById("map"), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+import { CustomMap } from './CustomMap';
+
+
+const user = new User();
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
