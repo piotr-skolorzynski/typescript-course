d77d3ad7d931513c909496d382f4c15e348ea794
Piotr <p.skolorzynski@gmail.com>
2022-01-09 23:04:09 +0100

    modify index.tsx create App class component with  onDecrement and onIncrement functions create interfaces for App component Add example of using functional component

--- react-redux-typescript/src/index.tsx
+++ react-redux-typescript/src/index.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
+interface AppProps {
+  color?: string;
+}
+
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  //state = { counter: 0 };  overrides state and do not generate problem by typescript we use this solution or constructor method
+
+  //constructor method do not override state property
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi there !</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+// // functional component
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+
+ReactDOM.render(<App color="red" />, document.querySelector("#root"));
