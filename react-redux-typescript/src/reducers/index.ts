5198d513efb473e5c13b64b426f6018fb5384a3f
Piotr <p.skolorzynski@gmail.com>
2022-01-10 21:43:20 +0100

    modify package.json *install axios *install react-redux *install redux *install redux-thunk
    modify index.ts
    *remove previous example
    *add create store
    *add wrapp App component with Provider with store
    
    create App component with basic structure
    
    create directory actions with index.ts and actions.ts files
    *create enum ActionTypes
    *create fetchTodos function with dispatch
    
    create directory reducers with index.ts and todos.ts files
    *todosReducer

---
+++ react-redux-typescript/src/reducers/index.ts
@@ -0,0 +1,11 @@
+import { todosReducer } from './todos';
+import { combineReducers } from "redux";
+import { Todo } from '../actions';
+
+export interface StoreState {
+    todos: Todo[];
+};
+
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducer
+});
