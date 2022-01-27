58c251cfa8bccc6338c95b18cbce57a981db846b
Piotr <p.skolorzynski@gmail.com>
2022-01-27 22:34:42 +0100

    create class Sorter, create constructor and sort metod

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,27 @@
 "use strict";
-console.log(123);
-const logSomething = () => {
-    console.log('something');
-};
-logSomething();
+//bubble sorting the simplest way and not too effective
+class Sorter {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //     this.collection = collection;
+    // }
+    // zamiennik tego co powyżej
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
