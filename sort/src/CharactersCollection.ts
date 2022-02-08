f82c67ee09253145c7570d8ec9b1d77b1aa1a95d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-02-08 22:02:22 +0100

    create and implement abstract class

--- sort/src/CharactersCollection.ts
+++ sort/src/CharactersCollection.ts
@@ -1,5 +1,9 @@
-export class CharactersCollection {
-  constructor(public data: string) {}
+import { Sorter } from "./Sorter";
+
+export class CharactersCollection extends Sorter {
+  constructor(public data: string) {
+    super();
+  }
 
   get length(): number {
     return this.data.length;
@@ -10,6 +14,7 @@ export class CharactersCollection {
       this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
     );
   }
+  
   swap(leftIndex: number, rightIndex: number): void {
     const characters = this.data.split('');
 
