762946fd518d8dd9f2db455ce917e22ee9f517ac
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-02-13 21:17:03 +0100

    modify index.ts file fix logic in for of loop
    modify CsvFileReader mofity read method using tuple to describe returned type

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,21 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
 const reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
-console.log(reader.data);
-var MatchResult;
-(function (MatchResult) {
-    MatchResult["HOME_WIN"] = "H";
-    MatchResult["AWAY_WIN"] = "A";
-    MatchResult["DRAW"] = "D";
-})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HOME_WIN) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HOME_WIN) {
         manUnitedWins += 1;
     }
-    else if (match[2] === 'Man United' && match[6] === MatchResult.AWAY_WIN) {
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AWAY_WIN) {
         manUnitedWins += 1;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,14 +3,13 @@ import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
-console.log(reader.data);
 
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HOME_WIN) {
     manUnitedWins += 1;
-  } else if (match[2] === 'Man United' && match[6] === MatchResult.AWAY_WIN) {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AWAY_WIN) {
     manUnitedWins += 1;
   }
 }
