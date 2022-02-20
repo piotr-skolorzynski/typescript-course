600b55da641177775b5ae5734e3ba50ce65dec1a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-02-20 22:27:09 +0100

    modify index.ts, MatchReader.ts and Summary.ts add static classes to create shortcuts to use in specific solutions

--- stats/build/index.js
+++ stats/build/index.js
@@ -2,17 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
-const WinsAnlysis_1 = require("./analyzers/WinsAnlysis");
+// import { WinsAnalysis } from './analyzers/WinsAnlysis';
 const Summary_1 = require("./Summary");
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
+// import { HtmlReport } from './reportTargets/HtmlReport';
 // create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // create an instance of MatchReader and pass in sth satysfying
 // the 'DataReader' interface
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+// shortcut using static method
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnlysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
-summary.buildAndPrintReport(matchReader.matches);
-const summary2 = new Summary_1.Summary(new WinsAnlysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   new ConsoleReport()
+// );
+// summary.buildAndPrintReport(matchReader.matches);
+// const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// summary2.buildAndPrintReport(matchReader.matches);
+// shortcut using static method for above specific type of report
+const summary2 = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 summary2.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,23 +1,31 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnlysis';
+// import { WinsAnalysis } from './analyzers/WinsAnlysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
+// import { HtmlReport } from './reportTargets/HtmlReport';
 
 // create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
 
 // create an instance of MatchReader and pass in sth satysfying
 // the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+
+// shortcut using static method
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
-summary.buildAndPrintReport(matchReader.matches);
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   new ConsoleReport()
+// );
+// summary.buildAndPrintReport(matchReader.matches);
 
-const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// summary2.buildAndPrintReport(matchReader.matches);
+
+// shortcut using static method for above specific type of report
+const summary2 = Summary.winsAnalysisWithHtmlReport('Man United');
 summary2.buildAndPrintReport(matchReader.matches);
