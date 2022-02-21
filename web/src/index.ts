c85c68384a8d8c575965cea601bfa7476396cd6a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-02-21 22:05:22 +0100

    modify User class add method set, modify interface UserProps

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,11 @@ import { User } from './models/User';
 const user = new User({ name: 'skolo', age: 39 });
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+user.set({ name: 'Stefan', age: 19 });
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ age: 40 });
+console.log(user.get('age'));
+console.log(user.get('name'));
