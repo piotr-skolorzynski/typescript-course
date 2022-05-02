e8402819c1fd9bbf313283f696aaca944ff57485
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-03 00:10:04 +0200

    refactoring Sync class

---
+++ web/src/models/Sync.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export const BASE_URL = 'http://localhost:3000/users';
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,13 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
-
-export const BASE_URL = 'http://localhost:3000/users';
 
 export class User {
   public events: Eventing = new Eventing();
@@ -20,23 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update); //it overwrites this.data with update object
-  }
-
-  fetch(): void {
-    axios
-      .get(`${BASE_URL}/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`${BASE_URL}/${id}`, this.data);
-    } else {
-      axios.post(`${BASE_URL}`, this.data);
-    }
   }
 }
