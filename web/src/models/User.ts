58a42e749c928f35e268bd3594b660e4f63d72c7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-03 12:09:57 +0200

    extract Atrributes class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -10,14 +10,4 @@ export interface UserProps {
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(BASE_URL);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update); //it overwrites this.data with update object
-  }
 }
