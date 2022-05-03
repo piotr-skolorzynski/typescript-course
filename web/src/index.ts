d4dc7b7498c94f14926bfbb059c352ab0a6ae11d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-03 22:28:52 +0200

    change passthrough methods to arrow functions to deal with context

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,17 +1,5 @@
 import { User } from './models/User';
 
-// const user = new User({ id: 1 });
+const user = new User({ name: 'skolo', age: 39 });
 
-// user.set({ name: 'maciek', age: 37 });
-
-// user.save();
-
-const user2 = new User({ name: 'skolo', age: 39 });
-
-// user2.save();
-
-user2.events.on('change', () => {
-  console.log('change!');
-});
-
-user2.events.trigger('change');
+console.log(user.get('name'));
