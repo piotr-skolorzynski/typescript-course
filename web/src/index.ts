60a473ec62f45cd6248f7e6c19b5250fcbb92e93
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-05 22:49:18 +0200

    shortened passthrough methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'mleko', age: 32 });
+const user = User.buildUser({ id: 2 });
 
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 });
 
