a753b068c63f6672ee8e70c52483cad550e6eac9
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-07 17:41:21 +0200

    generic User Collection

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,11 @@
 import { BASE_URL } from './models/ApiSync';
 import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const collection = new Collection(BASE_URL);
+const collection = new Collection<User, UserProps>(
+  BASE_URL,
+  (json: UserProps) => User.buildUser(json)
+);
 
 collection.on('change', () => {
   console.log(collection);
