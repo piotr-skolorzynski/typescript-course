3c057499f67191885b56429595246dbfeb9f4ed6
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-12 22:58:24 +0200

    finish class UserEdit

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,14 @@
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
+import { UserEdit } from './views/UserEdit';
 
 const user = User.buildUser({ name: 'NAME', age: 21 });
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
 } else {
   throw new Error('Root element not found');
 }
--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -2,6 +2,10 @@ import { User, UserProps } from '../models/User';
 import { View } from './View';
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return { userShow: '.user-show', userForm: '.user-form' };
+  }
+
   template(): string {
     return `
             <div>
