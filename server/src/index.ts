2d2e017cb0e3f4373371e985cdeb00370ad08a08
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-15 00:10:37 +0200

    using an express router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-        <div>
-            <h1>Hi there!</h1>
-        </div>
-    `);
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Listen on port 3000');
