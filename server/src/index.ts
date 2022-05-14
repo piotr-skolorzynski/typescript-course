bb22deeae769fb40fb9bd4538afc0a2f5369569d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-14 23:57:50 +0200

    basic routes with Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    `);
+});
+
+app.listen(3000, () => {
+  console.log('Listen on port 3000');
+});
