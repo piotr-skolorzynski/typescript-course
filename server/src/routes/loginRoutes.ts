29f10e3dd0e6bb271d43126a18bd1d207b8aa97b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-15 22:06:10 +0200

    check login status

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import e, { Request, Response, Router } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
@@ -32,6 +32,24 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     res.redirect('/');
   } else {
     res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
   }
 });
 
