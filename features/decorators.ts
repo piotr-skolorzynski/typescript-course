70dd8e130639a73c34022f3bd63dafcf0d2324f5
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-05-20 14:34:27 +0200

    class decorator and other

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,14 +1,37 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat color is ${this.color}`;
   }
 
   @logError('Ooops, boat was sunk in ocean')
-  pilot(): void {
-    throw new Error();
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  // console.log(target);
+  console.log(key);
 }
 
 function logError(errorMessage: string) {
@@ -28,5 +51,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
