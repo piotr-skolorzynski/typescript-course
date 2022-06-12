ba38e783271c2b55e8f664a6dc3edf56d2f9d026
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-06-12 13:02:09 +0200

    fix routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
-import { router } from './routes/loginRoutes';
 import './controllers/LoginController';
+import './controllers/RootController';
 import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asdadscxv'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
